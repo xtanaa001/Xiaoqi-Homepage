e5ee0b40085caf51c26d7c5296be7838dd733e30
xtanaa001 <xqtan.china@gmail.com>
2020-02-18 17:04:14 -0500

    2020

--- static/js/author-list.js
+++ static/js/author-list.js
@@ -1,32 +1,32 @@
 var authorList = {
 
     XQTan : {
-        name : "Xiaoqi Tan",
-        web : "https://xiaoqitan.org",
+        name : "<b>Xiaoqi Tan</b>",
+        web : "#",
     },
 
     Garcia : {
         name : "Alberto Leon-Garcia",
-        web : "https://www.nal.utoronto.ca/",
+        web : "#",
     },
 
     DHKTsang : {
         name : "Danny H.K. Tsang",
-        web : "https://eetsang.home.ece.ust.hk/",
+        web : "#",
     },
 
     YuanWu : {
         name : "Yuan Wu",
-        web : "https://www.fst.um.edu.mo/en/staff/yuanwu.html",
+        web : "#",
     },
 
     BSun : {
         name : "Bo Sun",
-        web : "http://c2e.ece.ust.hk/sunbo/",
+        web : "#",
     },
 
     Sareh : {
         name : "Sareh Agheb",
-        web : "http://c2e.ece.ust.hk/sunbo/",
+        web : "#",
     },
 };
