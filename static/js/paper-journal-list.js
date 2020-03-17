86af7430686ce9524a05297ee03ab8655bf73dac
xtanaa001 <xqtan.china@gmail.com>
2020-03-17 11:25:07 -0400

    2222

--- static/js/paper-journal-list.js
+++ static/js/paper-journal-list.js
@@ -9,13 +9,13 @@ var papers_journal = new Array (
         topic: "cloud",
         //paperAbstract : "<p></p>",
     },
-
+    
     {
         id : "network",
         name : "Bounded Efficiency Loss for Network Welfare Maximization under Arbitrary Arrivals",
-        coauthors : new Array(authorList.XQTan, authorList.Garcia),
+        coauthors : new Array(),
         journal : "Preprint",
-        highlight : "yes",
+        highlight : "no",
         topic: "network",
         //paperAbstract : "<p></p>",
     },
@@ -23,24 +23,24 @@ var papers_journal = new Array (
     {
         id : "continuous",
         name : "Online Job Scheduling with Time Flexibility and Diseconomy-of-scale Server Costs",
-        coauthors : new Array(authorList.XQTan, authorList.Garcia),
+        coauthors : new Array(),
         journal : "Preprint",
-        highlight : "yes",
+        highlight : "no",
         topic: "cloud",
         //paperAbstract : "<p></p>",
     },
 
+    */
+
     {
         id : "knapsack",
         name : "Online Knapsack Problems with Convex Packing Costs",
-        coauthors : new Array(authorList.XQTan, authorList.Garcia),
+        coauthors : new Array(authorList.XQTan, authorList.BSun, authorList.Garcia),
         journal : "Preprint",
         highlight : "yes",
         topic: "algorithms",
         //paperAbstract : "<p></p>",
     },
-
-    */
 
     {
         id : "unified",
