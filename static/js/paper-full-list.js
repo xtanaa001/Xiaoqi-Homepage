a5a3c2d3fc98bcfe3041f3c1db99ea95324b0025
xtanaa001 <xtanaa@gmail.com>
2025-04-13 22:00:30 -0600

    20250413

--- static/js/paper-full-list.js
+++ static/js/paper-full-list.js
@@ -28,7 +28,6 @@ var papers_full = new Array (
         note: "Track: Economics, Online Markets and Human Computation",
         paperAbstract : "<p>This paper addresses the online $k$-selection problem with diseconomies of scale (OSDoS), where a seller seeks to maximize social welfare by optimally pricing items for sequentially arriving buyers, accounting for increasing marginal production costs. Previous studies have investigated deterministic dynamic pricing mechanisms for such settings. However, significant challenges remain, particularly in achieving optimality with small or finite inventories and developing effective randomized posted price mechanisms. To bridge this gap, we propose a novel randomized dynamic pricing mechanism for OSDoS, providing a tighter lower bound on the competitive ratio compared to prior work. Our approach ensures optimal performance in small inventory settings (i.e., whenis small) and surpasses existing online mechanisms in large inventory settings (i.e., whenis large), leading to the best-known posted price mechanism for optimizing online selection and allocation with diseconomies of scale across varying inventory sizes. </p>",
     },
-
     
     {
         id : "staticPricing",
@@ -46,23 +45,6 @@ var papers_full = new Array (
     },
 
     {
-        id : "OSCC_TEAC_2025",
-        name : "Threshold Policies with Tight Guarantees for Online Selection with Convex Costs",
-        coauthors : new Array(authorList.XQTan, authorList.SYu, authorList.RBoutaba, authorList.Garcia),
-        journal : "ACM Transactions on Economics and Computation",
-        pubinfo: "[Forthcoming]",
-        //conference : "WINE 2023",
-        //arxiv: "https://arxiv.org/pdf/2310.06166.pdf",
-        pdf: "https://arxiv.org/pdf/2310.06166.pdf",
-        //talk: "/documents/OSCC_slides_WINE 2023.pdf",
-        highlight : "yes",
-        topic: "O",
-        year: "2025", 
-        note: "Earlier version was presented in WINE 2023",
-        paperAbstract : "<p>This paper provides threshold policies with tight guarantees for online selection with convex cost (OSCC). In OSCC, a seller wants to sell some asset to a sequence of buyers with the goal of maximizing his/her profit. The seller can produce additional units of the asset, but at non-decreasing marginal costs. At each time, a buyer arrives and offers a price, and the seller must make an immediate and irrevocable decision in terms of whether to accept the offer and produce/sell one unit of the asset to this buyer. The goal is to develop an online algorithm that selects a subset of buyers to maximize the seller’s profit, namely, the total selling revenue minus the total production cost. Our main result is the development of a class of simple threshold policies that are logistically-simple and easy to implement, but have provable optimality guarantees among all deterministic algorithms. We also derive a lower bound on competitive ratios of randomized algorithms, and prove that the competitive ratio of our threshold policy asymptotically converges to this lower bound when the total production output is sufficiently large. Our results generalize and unify various online search, pricing, and auction problems, and provide a new perspective on the impact of non-decreasing marginal costs on real-world online resource allocation problems. </p>",
-    },
-
-    {
         id : "GLB_subexp_tail",
         name : "Almost Free: Self-concordance in Natural Exponential Families and an Application to Bandits",
         coauthors : new Array(authorList.SLiu, authorList.AAyoub, authorList.FSentenac, authorList.XQTan, authorList.CSzepesvari),
@@ -75,5 +57,22 @@ var papers_full = new Array (
         year: "2024",
         //note: "Full version is available upon request",
         paperAbstract : "<p>We prove that single-parameter natural exponential families with subexponential tails are self-concordant with polynomial-sized parameters. For subgaussian natural exponential families we establish an exact characterization of the growth rate of the self-concordance parameter. Applying these findings to bandits allows us to fill gaps in the literature: We show that optimistic algorithms for generalized linear bandits enjoy regret bounds that are both second-order (scale with the variance of the optimal arm's reward distribution) and free of an exponential dependence on the bound of the problem parameter in the leading term. To the best of our knowledge, ours is the first regret bound for generalized linear bandits with subexponential tails, broadening the class of problems to include Poisson, exponential and gamma bandits.</p>",
+    },
+
+    {
+        id : "OSCC_TEAC_2025",
+        name : "Threshold Policies with Tight Guarantees for Online Selection with Convex Costs",
+        coauthors : new Array(authorList.XQTan, authorList.SYu, authorList.RBoutaba, authorList.Garcia),
+        //journal : "ACM Transactions on Economics and Computation",
+        //pubinfo: "vol. 13, no. 2, June 2025",
+        conference : "WINE 2023",
+        //arxiv: "https://arxiv.org/pdf/2310.06166.pdf",
+        pdf: "https://arxiv.org/pdf/2310.06166.pdf",
+        //talk: "/documents/OSCC_slides_WINE 2023.pdf",
+        highlight : "yes",
+        topic: "O",
+        year: "2023", 
+        note: "Journal version to appear in ACM Transactions on Economics and Computation",
+        paperAbstract : "<p>This paper provides threshold policies with tight guarantees for online selection with convex cost (OSCC). In OSCC, a seller wants to sell some asset to a sequence of buyers with the goal of maximizing his/her profit. The seller can produce additional units of the asset, but at non-decreasing marginal costs. At each time, a buyer arrives and offers a price, and the seller must make an immediate and irrevocable decision in terms of whether to accept the offer and produce/sell one unit of the asset to this buyer. The goal is to develop an online algorithm that selects a subset of buyers to maximize the seller’s profit, namely, the total selling revenue minus the total production cost. Our main result is the development of a class of simple threshold policies that are logistically-simple and easy to implement, but have provable optimality guarantees among all deterministic algorithms. We also derive a lower bound on competitive ratios of randomized algorithms, and prove that the competitive ratio of our threshold policy asymptotically converges to this lower bound when the total production output is sufficiently large. Our results generalize and unify various online search, pricing, and auction problems, and provide a new perspective on the impact of non-decreasing marginal costs on real-world online resource allocation problems. </p>",
     },
 );
